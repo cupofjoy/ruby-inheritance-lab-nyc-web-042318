4584d8015942b712e545125e00ef7c511112c500
cupofjoy <eelantung@gmail.com>
2018-04-27 09:01:15 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def self.knowledge
+    @knowledge
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge.each do |string|
+      puts string
+    end
+  end
 end
